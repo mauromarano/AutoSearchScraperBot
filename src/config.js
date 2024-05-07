3876f22f90f80f16e51922394f1c6100b48c01d3
mauromarano <gagginaspinnata@gmail.com>
2024-05-07 11:46:39 +0200

    rimosso file

--- src/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  token: "6267254508:AAH03iRy-727AMp48q7br6N1EUDqB_rHIj4",
-  chat_id: "41734766",
-  user: "postgres",
-  password: "admin",
-  host: "postgres",
-  db_name: "autoscout",
-};
